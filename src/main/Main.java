3ec525dee2179ecdeb03516134231dcda7eb8da6
samyboy89 <sam.mathias.weggersen@gmail.com>
2015-05-29 15:16:41 +0200

    Update main

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.TitledPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,7 +14,8 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
         try {
-            stage.setScene(new Scene(FXMLLoader.load(Main.class.getResource("view/view.fxml"))));
+            Pane root = FXMLLoader.load(Main.class.getResource("view/view.fxml"));
+            stage.setScene(new Scene(root));
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
